60b718788c380bea164b92a0fc3bfc95a50c0f5d
g-marshak <marshak22@up.edu>
2022-11-05 12:15:02 -0700

    Enemy Animation/Sprites

--- Enemy.gd
+++ Enemy.gd
@@ -5,7 +5,7 @@ var aggro = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Sprite.play("default")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
