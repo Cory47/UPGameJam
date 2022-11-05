ee17a07dd022816b522c41d1d42a3d0413121636
ReynHisamoto <hisamoto24@up.edu>
2022-11-05 11:14:08 -0700

    current enemies

---
+++ Enemies/Enemy.gd
@@ -0,0 +1,56 @@
+extends StaticBody
+
+var health = 100
+var aggro = false
+
+signal karma
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#makes sprite face player
+	var player_pos = get_player_pos()
+	look_at(player_pos, Vector3.UP)
+	
+	#standy/aggro behavior
+	if(aggro == false):
+		idle_behavior()
+	else:
+		aggro_behavior()
+		
+	
+		
+	if health < 0:
+		die()
+
+#placeholders for base class right now
+func idle_behavior():
+	pass
+	
+func aggro_behavior():
+	pass
+	
+func die():
+	emit_signal("karma")
+	queue_free()
+	
+#gets adjusted player position to rotate enemy sprite, ignores up/down rotation
+func get_player_pos():
+	var player_pos = $".".get_parent().get_node_or_null("Player").global_transform.origin
+	var adjusted_pos = Vector3(player_pos.x, $".".global_transform.origin.y, player_pos.z)
+	
+	return adjusted_pos
+
+#determines enemy behavior when player is nearby
+func _on_AggroRange_body_entered(body):
+	if(body.is_in_group("Player")):
+		aggro = true
+		print("player entered aggro range")
+func _on_AggroRange_body_exited(body):
+	if(body.is_in_group("Player")):
+		aggro = false
+		print("player exited aggro range")
