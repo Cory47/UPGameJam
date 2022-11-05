9a87022c8e4ccafd6bfa531523c26bf3a68f37d6
tashchuk23 <94871535+natalietash@users.noreply.github.com>
2022-11-05 12:43:34 -0700

    updating ammo on HUD

--- Levels/Main/HUD.gd
+++ Levels/Main/HUD.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+signal start_game
+var i = -200
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -10,7 +12,23 @@ extends CanvasLayer
 func _ready():
 	pass # Replace with function body.
 
+func _process(delta):
+	$HealthLabel.text = "Health: " + str(i)
+	$HealthBar.value = i
+	i = i + 1
 
+
+func update_score(score):
+	$ScoreLabel.text = str(score)
+	
+
+func update_ammo(ammo):
+	$NumBulletsLabel.text = "Ammo: " + str(ammo)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_HealthBar_value_changed(new_value):
+	i = i + 1
+	$HealthBar.value = i
